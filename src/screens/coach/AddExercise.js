e7cc1f621899245e2c73b9d5b8aada9f5647d167
rodrigosa7 <digex2008@live.com.pt>
2021-04-22 00:49:13 +0100

    Update AddExercise.js

--- src/screens/coach/AddExercise.js
+++ src/screens/coach/AddExercise.js
@@ -1,12 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {
-  Text,
-  View,
-  TextInput,
-  Image,
-  Button,
-  ImagePickerIOS,
-} from 'react-native';
+import {Text, View, TextInput, Image, Button, StyleSheet} from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import axios from 'axios';
 import ImagePicker from 'react-native-image-crop-picker';
@@ -17,6 +10,7 @@ export default ({props, route}) => {
   const [gesto, setGesto] = useState('');
   const [gestos, setGestos] = useState([]);
   const [imagem, setImagem] = useState('https://reactjs.org/logo-og.png');
+  const [imagemsend, setImagemSend] = useState(null);
   useEffect(() => {
     getGestos();
   });
@@ -36,6 +30,31 @@ export default ({props, route}) => {
     console.warn(descricao);
     console.warn(gesto);
     console.warn(imagem);
+
+    var formData = new FormData();
+
+    formData.append('nome', nome);
+    formData.append('desc', descricao);
+    formData.append('gesto', gesto);
+    //formData.append('foto', imagemsend);
+
+    formData.append('foto', {
+      name: 'upload',
+      type: imagemsend.mime,
+      uri:
+        Platform.OS === 'android'
+          ? imagemsend.path
+          : imagemsend.path.replace('file://', ''),
+    });
+
+    axios({
+      method: 'post',
+      url: 'http://volleyapi.sarapaiva.webtuga.net/exercise',
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+      data: formData,
+    });
   };
   escolherFoto = () => {
     ImagePicker.openPicker({
@@ -45,16 +64,27 @@ export default ({props, route}) => {
     }).then((image) => {
       console.log(image);
       setImagem(image.path);
+      setImagemSend(image);
     });
   };
 
   return (
     <View>
       <Text style={{fontSize: 20}}>Vamos adicionar exercicios</Text>
-      <Text>Nome: </Text>
-      <TextInput onChangeText={setNome} value={nome}></TextInput>
-      <Text>Descrição: </Text>
-      <TextInput onChangeText={setDescricao} value={descricao}></TextInput>
+      <View style={styles.container}>
+        <Text>Nome: </Text>
+        <TextInput
+          style={styles.inputsss}
+          onChangeText={setNome}
+          value={nome}></TextInput>
+      </View>
+      <View style={styles.container}>
+        <Text>Descrição: </Text>
+        <TextInput
+          style={styles.inputsss}
+          onChangeText={setDescricao}
+          value={descricao}></TextInput>
+      </View>
       <Text>Gesto: </Text>
       <Picker
         selectedValue={gesto}
@@ -70,10 +100,27 @@ export default ({props, route}) => {
           );
         })}
       </Picker>
-      <Text>Esquema: </Text>
-      <Image source={{uri: imagem}} style={{width: 250, height: 250}} />
+      <View>
+        <Text>Esquema: </Text>
+        <Image source={{uri: imagem}} style={{width: 250, height: 250}} />
+      </View>
       <Button title="Escolher Fotografia" onPress={escolherFoto}></Button>
       <Button title="Confirm" onPress={uploadExercise}></Button>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 40,
+    backgroundColor: '#EEE',
+    borderRadius: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  inputsss: {
+    width: '70%',
+    backgroundColor: '#FFF',
+  },
+});

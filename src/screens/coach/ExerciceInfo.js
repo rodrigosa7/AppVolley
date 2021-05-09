b40b89e916e4e4cfbf1a27ceeeda41f20601c15b
rodrigosa7 <digex2008@live.com.pt>
2021-05-10 00:26:31 +0100

    Update ExerciceInfo.js

--- src/screens/coach/ExerciceInfo.js
+++ src/screens/coach/ExerciceInfo.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import {
   Text,
   View,
+  Image,
   StyleSheet,
   FlatList,
   Button,
@@ -11,7 +12,16 @@ import {
 export default ({props, route}) => {
   return (
     <View>
-      <Text>nome do exercicio: {route.params.item.exdescricao}</Text>
+      <Text>nome do exercicio: {route.params.item.exnome}</Text>
+      <Text>descricao do exercicio: {route.params.item.exdescricao}</Text>
+      <Image
+        source={{
+          uri:
+            'https://sarapaiva.webtuga.net/uploadExercicios/' +
+            route.params.item.exurl,
+        }}
+        style={{width: 250, height: 250}}
+      />
     </View>
   );
 };

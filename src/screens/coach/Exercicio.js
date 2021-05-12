97ac71293b9b3dafbf3c9c77af4c0eb08cce287b
rodrigosa7 <digex2008@live.com.pt>
2021-05-12 14:19:09 +0100

    Filtros Corrigidos

--- src/screens/coach/Exercicio.js
+++ src/screens/coach/Exercicio.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Text,
   View,
@@ -7,13 +7,13 @@ import {
   Button,
   TouchableOpacity,
 } from 'react-native';
-import {Picker} from '@react-native-picker/picker';
+import { Picker } from '@react-native-picker/picker';
 import axios from 'axios';
 import server from '../../common';
 import Exercice from '../../components/Exercice';
 import ExerciceInfo from '../../screens/coach/ExerciceInfo';
 
-export default ({props, navigation}) => {
+export default ({ props, navigation }) => {
   const [gesto, setGesto] = useState('');
   const [exercicios, setExercicios] = useState([]);
 
@@ -31,30 +31,54 @@ export default ({props, navigation}) => {
       console.warn(e);
     }
   };
+
   addExercise = () => {
     navigation.navigate('AddExercise');
   };
+  filterExercise = async (tipoGesto) => {
+    setGesto(tipoGesto)
+    if (tipoGesto == "all") {
+      try {
+        const res = await axios.get(
+          `http://volleyapi.sarapaiva.webtuga.net/exercise`,
+        );
+        setExercicios(res.data);
+      } catch (e) {
+        console.warn(e);
+      }
+    } else {
+      try {
+        const res = await axios.get(`http://volleyapi.sarapaiva.webtuga.net/exercise/${tipoGesto}`);
+        setExercicios(res.data);
+      } catch (e) {
+        console.warn(e)
+      }
+    }
+
+
+  }
 
   return (
     <View style={styles.container}>
       <Picker
         selectedValue={gesto}
-        onValueChange={(itemValue) => setGesto(itemValue)}
+        onValueChange={(itemValue) => filterExercise(itemValue)}
         mode="dropdown">
+        <Picker.Item label="Todos" value="todos" />
         <Picker.Item label="Passe" value="passe" />
         <Picker.Item label="Remate" value="remate" />
         <Picker.Item label="Serviço" value="servico" />
       </Picker>
       <View style={styles.lista}>
-        <Text style={{fontSize: 20, marginTop: 20, marginLeft: 15}}>
+        <Text style={{ fontSize: 20, marginTop: 20, marginLeft: 15 }}>
           Lista de Exercicios
         </Text>
         <FlatList
           data={exercicios}
           keyExtractor={(item) => `${item.idExercicio}`}
-          renderItem={({item}) => (
+          renderItem={({ item }) => (
             <TouchableOpacity
-              onPress={() => navigation.navigate('InfoExercicio', {item})}>
+              onPress={() => navigation.navigate('InfoExercicio', { item })}>
               <Exercice {...item} />
             </TouchableOpacity>
           )}

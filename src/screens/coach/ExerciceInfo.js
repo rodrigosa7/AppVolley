daeda8f0114bf6c4834671e2e6ad7be267a722a2
Bernardo Alves <bjca1998@gmail.com>
2021-05-12 23:48:53 +0100

    Card Exerciso info

--- src/screens/coach/ExerciceInfo.js
+++ src/screens/coach/ExerciceInfo.js
@@ -9,20 +9,59 @@ import {
   TouchableOpacity,
   SafeAreaView,
 } from 'react-native';
+import {SizedBox} from 'sizedbox';
 
 export default ({props, route}) => {
   return (
-    <SafeAreaView>
-      <Text>nome do exercicio: {route.params.item.exnome}</Text>
-      <Text>descricao do exercicio: {route.params.item.exdescricao}</Text>
-      <Image
-        source={{
-          uri:
-            'https://sarapaiva.webtuga.net/uploadExercicios/' +
-            route.params.item.exurl,
-        }}
-        style={{width: 250, height: 250}}
-      />
+    <SafeAreaView style={styles.container}>
+      <SizedBox vertical={10} />
+      <Text
+        style={{
+          fontSize: 30,
+          marginTop: 20,
+          marginLeft: 15,
+          marginBottom: 15,
+        }}>
+        {route.params.item.exnome}
+      </Text>
+
+      <SizedBox vertical={20} />
+      <View style={styles.img}>
+        <Image
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          source={{
+            uri:
+              'https://sarapaiva.webtuga.net/uploadExercicios/' +
+              route.params.item.exurl,
+          }}
+          style={{width: 250, height: 250}}
+        />
+      </View>
+      <SizedBox vertical={10} />
+
+      <Text style={styles.title}>Descricao do exercicio: </Text>
+      <SizedBox horizontal={10} />
+      <Text style={styles.text}>{route.params.item.exdescricao}</Text>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#ffff',
+  },
+  img: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  text: {
+    marginLeft: 20,
+  },
+});

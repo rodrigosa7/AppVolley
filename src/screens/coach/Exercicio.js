5df491a4d8425703f70de4aaacd514d548f088d0
Bernardo Alves <bjca1998@gmail.com>
2021-05-12 21:12:16 +0100

    Sizebox and flex exercicios

--- src/screens/coach/Exercicio.js
+++ src/screens/coach/Exercicio.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   Text,
   View,
@@ -6,14 +6,16 @@ import {
   FlatList,
   Button,
   TouchableOpacity,
-  SafeAreaView
+  SafeAreaView,
 } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
+import {SizedBox} from 'sizedbox';
+import {Picker} from '@react-native-picker/picker';
 import axios from 'axios';
 import Exercice from '../../components/Exercice';
 import Icon from 'react-native-vector-icons/FontAwesome5';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-export default ({ props, navigation }) => {
+export default ({props, navigation}) => {
   const [gesto, setGesto] = useState('');
   const [exercicios, setExercicios] = useState([]);
   const [count, setCount] = useState(0);
@@ -36,8 +38,8 @@ export default ({ props, navigation }) => {
     navigation.navigate('AddExercise');
   };
   filterExercise = async (tipoGesto) => {
-    setGesto(tipoGesto)
-    if (tipoGesto == "todos") {
+    setGesto(tipoGesto);
+    if (tipoGesto == 'todos') {
       try {
         const res = await axios.get(
           `http://volleyapi.sarapaiva.webtuga.net/exercise`,
@@ -48,56 +50,58 @@ export default ({ props, navigation }) => {
       }
     } else {
       try {
-        const res = await axios.get(`http://volleyapi.sarapaiva.webtuga.net/exercise/${tipoGesto}`);
+        const res = await axios.get(
+          `http://volleyapi.sarapaiva.webtuga.net/exercise/${tipoGesto}`,
+        );
         setExercicios(res.data);
       } catch (e) {
-        console.warn(e)
+        console.warn(e);
       }
     }
-
-
-  }
+  };
 
   return (
     <SafeAreaView style={styles.container}>
-      <Picker
-        selectedValue={gesto}
-        onValueChange={(itemValue) => filterExercise(itemValue)}
-        mode="dropdown">
-        <Picker.Item label="Todos" value="todos" />
-        <Picker.Item label="Passe" value="passe" />
-        <Picker.Item label="Remate" value="remate" />
-        <Picker.Item label="Serviço" value="servico" />
-      </Picker>
-      <View style={styles.lista}>
-        <Text style={{ fontSize: 20, marginTop: 20, marginLeft: 15 }}>
+      <View style={styles.item1}>
+        <Text style={{fontSize: 40, marginTop: 20, marginLeft: 15}}>
           Lista de Exercicios
         </Text>
-        <FlatList
-          data={exercicios}
-          keyExtractor={(item) => `${item.idExercicio}`}
-          renderItem={({ item }) => (
-            <TouchableOpacity
-              onPress={() => navigation.navigate('InfoExercicio', { item })}>
-              <Exercice {...item} />
-            </TouchableOpacity>
-          )}
-        />
+
+        <View style={styles.lista}>
+          <FlatList
+            data={exercicios}
+            keyExtractor={(item) => `${item.idExercicio}`}
+            renderItem={({item}) => (
+              <TouchableOpacity
+                onPress={() => navigation.navigate('InfoExercicio', {item})}>
+                <Exercice {...item} />
+              </TouchableOpacity>
+            )}
+          />
+        </View>
+
+        <View styles={styles.button}>
+          <TouchableOpacity onPress={addExercise}>
+            <View style={styles.button}>
+              <Text>Adicionar Exercicio</Text>
+              <Icon name="volleyball-ball"></Icon>
+            </View>
+          </TouchableOpacity>
+        </View>
       </View>
-
-
-      <View styles={styles.botoes}>
-        <TouchableOpacity onPress={addExercise}>
-
-          <View
-            style={styles.button}>
-            
-            <Text>
-              Adicionar Exercicio
-            </Text>
-            <Icon name="dumbbell"></Icon>
-          </View>
-        </TouchableOpacity>
+      <View style={styles.item2}>
+        <Text style={{fontSize: 20, marginTop: 20, marginLeft: 15}}>
+          Filtro:
+        </Text>
+        <Picker
+          selectedValue={gesto}
+          onValueChange={(itemValue) => filterExercise(itemValue)}
+          mode="dropdown">
+          <Picker.Item label="Todos" value="todos" />
+          <Picker.Item label="Passe" value="passe" />
+          <Picker.Item label="Remate" value="remate" />
+          <Picker.Item label="Serviço" value="servico" />
+        </Picker>
       </View>
     </SafeAreaView>
   );
@@ -105,15 +109,23 @@ export default ({ props, navigation }) => {
 
 const styles = StyleSheet.create({
   button: {
-    
     backgroundColor: '#da581e',
     marginTop: 10,
     padding: 10,
- 
+    flexDirection: 'column',
     alignItems: 'center',
     borderRadius: 7,
-    width: "50%",
+    width: '50%',
     alignSelf: 'center',
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'flex-start',
   },
-
+  item1: {
+    height: '70%',
+  },
+  item2: {
+    height: '30%',
+  },
 });

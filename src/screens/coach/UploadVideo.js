b0808c70f78c46fd7cba2f31064ad01360e32ba1
rodrigosa7 <digex2008@live.com.pt>
2021-06-24 14:48:24 +0100

    Update UploadVideo.js

--- src/screens/coach/UploadVideo.js
+++ src/screens/coach/UploadVideo.js
@@ -1,11 +1,13 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 
 import {Picker} from '@react-native-picker/picker';
 import axios from 'axios';
+import {useIsFocused} from '@react-navigation/native';
 
 export default (props, {navigation}) => {
-  const [gesto, setGesto] = useState('');
+  const [gesto, setGesto] = useState('Todos');
+  const [exercicio, setExercicio] = useState(0);
   const [exercicios, setExercicios] = useState([]);
   const gestos = [
     {label: 'Passe', value: 'Passe'},
@@ -14,8 +16,26 @@ export default (props, {navigation}) => {
     {label: 'Bloco', value: 'Bloco'},
   ];
 
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    isFocused && getExercicios();
+  }, [isFocused]);
+
+  getExercicios = async () => {
+    try {
+      const res = await axios.get(
+        `http://volleyapi.sarapaiva.webtuga.net/exercise`,
+      );
+      console.log(res.status);
+      setExercicios(res.data);
+    } catch (e) {
+      console.warn(e);
+      console.log(e);
+    }
+  };
+
   filterExercise = async (tipoGesto) => {
-    setGesto(tipoGesto);
     if (tipoGesto == 'Todos') {
       try {
         const res = await axios.get(
@@ -60,6 +80,23 @@ export default (props, {navigation}) => {
             <Picker.Item label="Remate" value="Remate" />
             <Picker.Item label="Serviço" value="Serviço" />
             <Picker.Item label="Bloco" value="Bloco" />
+          </Picker>
+        </>
+      )}
+      {Platform.OS === 'android' && (
+        <>
+          <Text style={[styles.text, {marginLeft: 15}]}>Exercicio:</Text>
+          <Picker
+            selectedValue={exercicio}
+            style={{height: 50, width: 150}}
+            onValueChange={(itemValue, itemIndex) => filterExercise(itemValue)}>
+            {exercicios.map((item) => (
+              <Picker.Item
+                key={item.idExercicio}
+                label={item.exnome}
+                value={item.idExercicio}
+              />
+            ))}
           </Picker>
         </>
       )}
